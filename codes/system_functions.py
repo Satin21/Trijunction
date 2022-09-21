7f60ab189c46b5154168dd39cdf2ba8ff659d371
Juan Daniel Torres - io <jtorresluna@tudelft.nl>
2022-09-22 00:05:09 +0200

    update execution functions

--- codes/system_functions.py
+++ codes/system_functions.py
@@ -1,9 +1,17 @@
 from typing import Dict, Tuple
 from codes.trijunction_matrices import make_system
 from codes.utils import eigsh
+from toolz.functoolz import memoize
 
 
+@memoize
 def base_ham(parameters: Dict, trijunction, f_params):
+    """
+    Calculate the Hamiltonian of a trijunction using a given set
+    of parameters and a position dependent function.
+
+    The result is stored in memory to save time.
+    """
 
     ham = trijunction.hamiltonian_submatrix(
         sparse=True, params=f_params(**parameters)
@@ -11,33 +19,51 @@ def base_ham(parameters: Dict, trijunction, f_params):
 
     return ham
 
-
 def adaptive_two_parameters(
-    xy: Tuple[float],
-    voltages: Dict,
-    params: Dict,
-    gates: Tuple[str]
+    xy,
+    voltages,
+    gates
 ):
     """
     Energy of the first non-zero eigenvalue.
+    `gates` can be 'left', 'right', 'top', 'accum'.
     """
     
-    zero_potential, linear_terms, trijunction, f_params = make_system()
-    
-    params.update(potential=zero_potential)
-    num_ham = base_ham(params, trijunction, f_params)
+    kwant_params = make_system()
 
-    for i, gate in enumerate(gates):
-        voltages[gate+'_1'] = xy[i]
-        voltages[gate+'_2'] = xy[i]
+    for gate in enumerate(gates):
+        if gate in ['left', 'right', 'top']:
+            voltages[gate+'_1'] = xy[i]
+            voltages[gate+'_2'] = xy[i]            
+        elif gate == 'accum':
+            voltages[gate] = xy[i]
+
+    evals = diagonalisation(
+        new_param,
+        kwant_params,
+        voltages,
+        nevals=6
+    )
+    
+    return evals[-1]
+
+
+def diagonalisation(
+    new_param, kwant_params, voltages, nevals=20
+):
+    """
+    
+    """
+    trijunction, f_params, params, linear_terms = kwant_params
+    params.update(new_param)
 
     linear_ham = sum(
-        [
-            value * linear_terms[key]
-            for key, value in voltages.items()
-        ]
-    )
+            [
+                voltages[key] * linear_terms[key]
+                for key, value in voltages.items()
+            ]
+        )
 
-    evals = eigsh(linear_ham + num_ham, 6)
+    num_ham = base_ham(trijunction, f_params, params)
 
-    return evals[-1]
+    return eigsh(num_ham + linear_ham,  nevals)
