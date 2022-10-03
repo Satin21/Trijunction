d2e63bb072e3d0067b1b268ca4576e4d00f32761
Juan Daniel Torres - io <jtorresluna@tudelft.nl>
2022-10-03 10:00:26 +0200

    update args

--- codes/system_functions.py
+++ codes/system_functions.py
@@ -1,20 +1,6 @@
 from typing import Dict, Tuple
-from codes.trijunction_matrices import make_system
 from codes.utils import eigsh
 from toolz.functoolz import memoize
-
-
-def base_ham(trijunction, f_params, parameters):
-    """
-    Calculate the Hamiltonian of a trijunction using a given set
-    of parameters and a position dependent function.
-
-    The result is stored in memory to save time.
-    """
-
-    ham = trijunction.hamiltonian_submatrix(sparse=True, params=f_params(**parameters))
-
-    return ham
 
 
 def adaptive_two_parameters(xy, voltages, gates, params, kwant_params):
@@ -41,15 +27,16 @@ def adaptive_two_parameters(xy, voltages, gates, params, kwant_params):
     return evals[-1]
 
 
-def diagonalisation(new_param, kwant_params, voltages, params, nevals=20):
-    """ """
+def diagonalisation(new_param, kwant_params, params, nevals=20):
+    """
+    """
     trijunction, f_params, linear_terms = kwant_params
     params.update(new_param)
 
     linear_ham = sum(
-        [voltages[key] * linear_terms[key] for key, value in voltages.items()]
+        [params[key] * linear_terms[key] for key in linear_terms.keys()]
     )
 
-    num_ham = base_ham(trijunction, f_params, params)
+    num_ham = trijunction.hamiltonian_submatrix(sparse=True, params=f_params(**params))
 
     return eigsh(num_ham + linear_ham, nevals)
