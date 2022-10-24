885baf02f3a85106f18588ccc7aada52c4c9e3c1
Juan Daniel Torres - io <jtorresluna@tudelft.nl>
2022-10-24 19:00:38 +0200

    recover file

--- codes/optimization.py
+++ codes/optimization.py
@@ -7,13 +7,7 @@ import kwant
 sys.path.append("/home/tinkerer/spin-qubit/")
 
 from codes.tools import hamiltonian
-from codes.constants import (
-    scale,
-    majorana_pair_indices,
-    voltage_keys,
-    bands,
-    topological_gap,
-)
+from codes.constants import scale, majorana_pair_indices, voltage_keys, bands, topological_gap
 from codes.parameters import junction_parameters, dict_update, phase_pairs, voltage_dict
 from codes.utils import wannierize, svd_transformation, eigsh
 
@@ -21,32 +15,36 @@ from potential import gate_potential, linear_problem_instance
 from Hamiltonian import discrete_system_coordinates
 from utility import gather_data
 from dask import delayed
+from scipy.sparse._coo import coo_matrix
 
 
 def loss(x, *argv):
     """
-    Loss function used to optimise the coupling between pairs of MBSs.
-    It can be used to optimize volateges or phases.
-    Parameters
-    ----------
-    x: either list or scalar (float)
-        list when optimizing voltages and float when optimizing phases
-    argv: argument for the loss functions
-        pair: str
-            describes the pair to be coupled
-        params: dict
-            contains parameters for the hamiltonian, voltages, and dep_acc_index
-        kwant_system, f_params, linear_terms, reference_wave_functions: tuple
-            contains parameters required for `majorana_loss`
-    Returns
-    -------
-    shape_loss: `soft_threshold` if it is not zero
-    majorana_loss: if `soft_threshold` is zero
+        Loss function used to optimise the coupling between pairs of MBSs.
+        It can be used to optimize volateges or phases.
+        Parameters
+        ----------
+        x: either list or scalar (float)
+            list when optimizing voltages and float when optimizing phases
+        argv: argument for the loss functions
+            pair: str
+                describes the pair to be coupled
+            params: dict
+                contains parameters for the hamiltonian, voltages, and dep_acc_index
+            kwant_system, f_params, linear_terms, reference_wave_functions: tuple
+                contains parameters required for `majorana_loss`
+        Returns
+        -------
+        shape_loss: `soft_threshold` if it is not zero
+        majorana_loss: if `soft_threshold` is zero
     """
+    
+    # print(x)
 
     pair = argv[0]
     params = argv[1]
-    system, linear_terms, f_params, density_operator, reference_wave_functions = argv[2]
+    system, linear_terms, f_params, densityoperator, reference_wavefunctions = argv[2]
+
 
     if isinstance(x, (list, np.ndarray)):
         new_parameter = voltage_dict(x)
@@ -54,182 +52,212 @@ def loss(x, *argv):
         new_parameter = phase_pairs(pair, x * np.pi)
 
     params.update(new_parameter)
+    
+    linear_ham, full_hamiltonian = hamiltonian(
+            system, linear_terms, f_params, **params
+        )
+    
+    energies, wavefunctions = eigsh(
+            full_hamiltonian.tocsc(),
+            len(reference_wavefunctions),
+            sigma=0,
+            return_eigenvectors=True,
+        )
+
 
     # Uncomment this in case of soft-thresholding
     cost = 0
     if isinstance(x, (list, np.ndarray)):
-        args = (
-            pair.split("-"),
-            (system, linear_terms, density_operator),
-            params["dep_acc_index"],
-        )
-        potential_shape_loss = soft_threshold_loss(x, *args)
+        
+        args = (pair.split('-'),
+                densityoperator, 
+                params['dep_acc_index'],
+                (10, 1)
+               )
 
-        cost += potential_shape_loss
-
-    linear_ham, full_hamiltonian = hamiltonian(system, linear_terms, f_params, **params)
-
-    cost += majorana_loss(full_hamiltonian, reference_wave_functions) / topological_gap
+        cost += wavefunction_loss(wavefunctions, 
+                                  *args
+                                 )
+        
+    cost += majorana_loss(energies,
+                          wavefunctions,
+                          reference_wavefunctions)/topological_gap
 
     return cost
 
 
-def soft_threshold_loss(x, *argv):
-
+def shape_loss(x, *argv):
+    
     """
     Computes total loss from the potential shape and wavefunction thresholding
-
+    
     Input
     -----
     x: 1xn array
     Input voltages
-
+    
     argv: tuple
     arguments required for soft thresholding such as
     (coupled pair:list, (base hamiltonian, linear ham, kwant density operator), indices:dict)
-
+    
     Returns
     -------
     loss: float
-
+    
     """
-    print(x)
+    # print(x)
+    
     pair = argv[0]
     system, linear_terms, density_operator = argv[1]
     indices = argv[2]
-
+    
     voltages = voltage_dict(x)
 
-    linear_ham, full_hamiltonian = hamiltonian(system, linear_terms, **voltages)
+    linear_ham, full_hamiltonian = hamiltonian(
+        system, linear_terms, **voltages
+    )
     linear_ham = linear_ham.diagonal()[::4]
-    potential_shape_loss = shape_loss(indices, linear_ham, pair)
-
-    if potential_shape_loss < 1e-9:
-        evals, evecs = eigsh(full_hamiltonian, k=6, sigma=0, return_eigenvectors=True)
-        return wavefunction_loss(evecs, density_operator, indices, pair)
-    return potential_shape_loss
-
-
-def shape_loss(indices, linear_ham, pair, mu=bands[0]):
-    """
-    Parameters
-    ----------
-    indices: dict
-        dictionary coming from `dep_acc_indices`
-    linear_terms: ndarray
-        reduced diagonal of voltage matrix
-    pair: ndarray
-        array with names of the channels to connect
-    mu: float
-        reference chemical potential
-
-    Returns
-    -------
-    loss function of shape
-    """
-    loss = 0
-
-    # loss = {}
-
+    
+    loss = 0.0
+    
     for gate, index in indices.items():
-        diff = np.real(linear_ham[index]) - mu
-
+        diff = np.real(linear_ham[index]) - bands[0]
         if gate[:-1] in pair:
             if diff > 0:
                 loss += diff
+
         else:
             if diff < 0:
                 loss += diff
+
     return np.abs(loss)
 
 
-def wavefunction_loss(wfs, densityoperator, indices, pair, ci=50):
+
+def wavefunction_loss(x, *argv):
     """
     Soft-thresholding with wavefunction
-
+    
     Input
     -----
-    wfs: nx1 array
+    x: nx1 array
     Wavefunction
-
+    
     densityoperator: kwant operator
-
+    
     indices: dict
     Values are the indices corresponding to the position at which the and wavefunction probability is evaluated.
-    Keys are the region names. Channel indices are represented as `left0`, `right0`, `top0`, whereas regions below
+    Keys are the region names. Channel indices are represented as `left0`, `right0`, `top0`, whereas regions below 
     the gates are represented as `left_1`, `right_1`, `top_1` (with an underscore).
-
+    
     pair: list
     Channel names those need to be coupled.
-
+    
     ci: int
     Confidence interval (%) for the relative magnitude of the wavefunction density across two channels to be coupled.
-
-
+    
+    
     Returns
     -------
     loss: float
     """
 
-    def return_gain(pair, index, wf):
-        gain = dict.fromkeys(pair, [])
-        for gate, ind in index.items():
-            if gate[:-1] in pair:
-                a = gain[gate[:-1]].copy()
-                a.append(np.abs(wf[ind]))
-                gain.update({gate[:-1]: a})
-        return gain
+    if len(x.shape) == 1:
 
-    gain = np.vstack(
-        [
-            list(return_gain(pair, indices, densityoperator(wfs[:, i])).values())
-            for i in range(3)
-        ]
-    )
-    gain = [np.sum(gain[0::2], axis=0), np.sum(gain[1::2], axis=0)]
-    sum_gain = np.sum(gain, axis=1)
+        system, params, linear_terms, f_params, densityoperator = argv[0]
+        pair, indices, (ci, wf_amp) = argv[1]
 
-    if np.all(sum_gain > 1e-5) and (
-        (1 - ci / 100) < sum_gain[0] / sum_gain[1] < (1 + ci / 100)
-    ):
+        params.update(voltage_dict(x))
+
+        _, full_hamiltonian = hamiltonian(
+                system, linear_terms, f_params, **params
+            )
+        
+        _, wfs = eigsh(
+                full_hamiltonian.tocsc(),
+                6,
+                sigma=0,
+                return_eigenvectors=True,
+            )
+        
+    else:
+        pair, densityoperator, indices, (ci, wf_amp) = argv
+        wfs = x
+    
+    amplitude = lambda i: _amplitude(pair, indices, densityoperator(wfs[:, i]))
+    
+    desired, undesired = [], []
+    for i in range(3):
+        des, undes = amplitude(i)
+        desired.append(list(des.values()))
+        undesired.append(undes)
+        
+    desired = np.vstack(desired)
+
+    desired = [np.sum(desired[0::2], axis=0), 
+               np.sum(desired[1::2], axis=0)
+              ]
+    
+    sum_desired = np.sum(desired, axis = 1)
+    rel_amplitude = sum_desired[0]/sum_desired[1]
+    
+    left_bound = (1-ci/100)
+    right_bound = (1+ci/100)
+    
+    # print(sum_desired, np.abs(np.sum(np.diff(desired, axis=0))), np.sum(undesired))
+    
+    if np.all(sum_desired > wf_amp) and (left_bound<rel_amplitude<right_bound):
         return -1
-    return -(sum(sum_gain)) + np.sum(np.diff(gain, axis=0))
+    
+    return (
+        - sum(sum_desired)
+        + np.abs(np.sum(np.diff(desired, axis=0)))
+        + 1e1 * np.sum(undesired)
+    )
 
+def _amplitude(pair, index, wf):
+    desired = dict.fromkeys(pair, [])
+    undesired = []
+    for gate, ind in index.items():
+        if gate[:-1] in pair:
+            temp = desired[gate[:-1]].copy()
+            temp.append(np.abs(wf[ind]))
+            desired.update({gate[:-1]: temp})
 
-def majorana_loss(numerical_hamiltonian, reference_wave_functions):
+        else:
+            undesired.append(np.abs(wf[ind]))
+    return desired, undesired
+ 
+
+def majorana_loss(
+    energies, wavefunctions, reference_wavefunctions
+):
     """Compute the quality of Majorana coupling in a Kwant system.
 
     Parameters
     ----------
-    x : 1d array
-        The vector of parameters to optimize
-    numerical_hamiltonian : coo matrix
-        A function for returning the sparse matrix Hamiltonian given parameters.
-    reference_wave_functions : 2d array
+    energies : 1d array
+        Eigenenergies
+    wavefunctions: 2d array
+        Eigenvectors
+    reference_wavefunctions : 2d array
         Majorana wave functions. The first two correspond to Majoranas that
         need to be coupled.
     scale : float
         Energy scale to use.
     """
 
-    energies, wave_functions = eigsh(
-        numerical_hamiltonian.tocsc(),
-        len(reference_wave_functions),
-        sigma=0,
-        return_eigenvectors=True,
-    )
 
-    transformed_hamiltonian = svd_transformation(
-        energies, wave_functions, reference_wave_functions
+    transformed_hamiltonian = (
+        svd_transformation(energies, wavefunctions, reference_wavefunctions)
     )
 
     desired = np.abs(transformed_hamiltonian[0, 1])
     undesired = np.linalg.norm(transformed_hamiltonian[2:], ord=1)
 
-    print(desired, undesired)
-
+    # print(desired, undesired)
+    
     return -desired + undesired
-
 
 def jacobian(x0, *args):
 
